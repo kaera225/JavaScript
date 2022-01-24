9a42e37de6bec9c7968d0f801d95e386cf3a66df
kaera225 <75858949+kaera225@users.noreply.github.com>
2022-01-24 17:12:23 +0000

    Add files via upload

--- COURS JS/api/script.js
+++ COURS JS/api/script.js
@@ -1,8 +1,16 @@
 const quote = document.getElementById('quote');
+const getQuote = () => {
+    fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+            //   console.log(`${data.content}-${data.author}`)
+            quote.innerHTML = data.content;
+        });
+    fetch('https://picsum.photos/1600/1000')
+        .then((response) => {
+            document.getElementById('picture').innerHTML = `<img src=${response.url} />`;
+        });
+}
+quote.addEventListener('click', () => getQuote());
 
-fetch('https://api.quotable.io/random')
-    .then((response) => response.json())
-    .then((data) => {
-        //   console.log(`${data.content}-${data.author}`)
-        quote.innerHTML = data.content;
-    });
+
