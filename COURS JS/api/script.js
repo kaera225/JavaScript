eb3b793e5f158a10e128469ea1531653229948b1
kaera225 <75858949+kaera225@users.noreply.github.com>
2022-01-24 15:56:23 +0000

    Add files via upload

--- COURS JS/api/script.js
+++ COURS JS/api/script.js
@@ -1,1 +1,8 @@
 const quote = document.getElementById('quote');
+
+fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+        //   console.log(`${data.content}-${data.author}`)
+        quote.innerHTML = data.content;
+    });
